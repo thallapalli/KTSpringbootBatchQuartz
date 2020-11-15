22f62208bcbd4e79285eb11d749cb7a461e881a6
293207 <293207@LTUS68130.cts.com>
2020-11-14 19:59:18 -0500

    H2 console enabled

--- src/main/java/com/kt/batch/quartz/poc/job/SpringBatchJobConfiguration.java
+++ src/main/java/com/kt/batch/quartz/poc/job/SpringBatchJobConfiguration.java
@@ -3,7 +3,6 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +12,7 @@ import com.kt.batch.quartz.poc.writer.CustomItemWriter;
 
 @Configuration
 
-public class SpringBatchConfiguration {
+public class SpringBatchJobConfiguration {
 	@Autowired
 	 public JobBuilderFactory jobBuilderFactory;
 	 
